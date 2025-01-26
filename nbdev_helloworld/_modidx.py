b1e44b1807a3a340a6b774754e9ad8ac590dc697
REDRUP-LAPTOP01 <matthew.redrup@gmail.com>
2025-01-26 23:47:56 +1100

    Add `say_hello`; update index

--- nbdev_helloworld/_modidx.py
+++ nbdev_helloworld/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://LotsOfOrg.github.io',
                 'git_url': 'https://github.com/LotsOfOrg/nbdev-helloworld',
                 'lib_path': 'nbdev_helloworld'},
-  'syms': {'nbdev_helloworld.core': {'nbdev_helloworld.core.foo': ('core.html#foo', 'nbdev_helloworld/core.py')}}}
+  'syms': { 'nbdev_helloworld.core': {'nbdev_helloworld.core.foo': ('core.html#foo', 'nbdev_helloworld/core.py')},
+            'nbdev_helloworld.hello': {'nbdev_helloworld.hello.say_hello': ('core.html#say_hello', 'nbdev_helloworld/hello.py')}}}
